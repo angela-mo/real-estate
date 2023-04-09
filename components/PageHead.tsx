7be4405669c1b58acd4be27b125aa21b719e9361
Angela Mo <angelamo2413@gmail.com>
2023-04-09 15:30:38 -0400

    Update PageHead.tsx
    edited page head text

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -35,11 +35,11 @@ const PageHead = () => {
 			<meta property="twitter:url" content="https://metatags.io/" />
 			<meta
 				property="twitter:title"
-				content="Mo's Real Estate - The best of the best"
+				content="Mo's Real Estate - The Best of The Best"
 			/>
 			<meta
 				property="twitter:description"
-				content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+				content="Your go-to site for a real estate hunt!"
 			/>
 			<meta
 				property="twitter:image"
